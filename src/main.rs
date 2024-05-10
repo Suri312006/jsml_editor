8502e3cba31aef0bb9eb77a427e064f685b26aa8
suri312006 <suri312006@gmail.com>
2024-05-10 10:04:39 -0700

    working on the thing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,121 @@
+use crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+
+use ratatui::backend::{Backend, CrosstermBackend};
+use ratatui::Terminal;
+
+use std::io;
+
+use app::{App, CurrentScreen, CurrentlyEditing};
 mod app;
 mod ui;
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //setup terminal
+    enable_raw_mode();
+
+    let mut stderr = io::stderr();
+
+    // using stderr because we want to output the json into stdout
+    // such as piping
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stderr);
+
+    let mut term = Terminal::new(backend);
+
+    // creating and running the app
+    let mut app = App::new();
+    let res = run_app(&mut term, &mut app);
+
+    disable_raw_mode()?;
+    execute!(
+        term.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    term.show_cursor()?;
+
+    //checks if run_app errored, and handles it accordingly
+    if let Ok(do_print) = res {
+        if do_print {
+            app.print_json()?;
+        }
+    } else if let Err(err) = res {
+        println!("{err:?}");
+    }
+
+    Ok(())
+}
+
+// function is generic across the Backend type, makes code backend agnostic
+// so that means that it can run across all backends that implement that trait, even custom ones.
+fn run_app<B: Backend>(term: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+    loop {
+        term.draw(|f| ui(f, app))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // skip events that arent pressevents
+                continue;
+            }
+
+            match app.current_screen {
+                CurrentScreen::Main => match key.code {
+                    KeyCode::Char('e') => {
+                        app.current_screen = CurrentScreen::Editing;
+                        app.currently_editing = Some(app::CurrentlyEditing::Key);
+                    }
+                    KeyCode::Char('q') => {
+                        app.current_screen = CurrentScreen::Exiting;
+                    }
+
+                    _ => {}
+                },
+
+                CurrentScreen::Exiting => match key.code {
+                    KeyCode::Char('y') => {
+                        return Ok(true);
+                    }
+                    KeyCode::Char('n') => {
+                        return Ok(false);
+                    }
+                    _ => {}
+                },
+
+                CurrentScreen::Editing if key.kind == KeyEventKind::Press => match key.code {
+                    KeyCode::Enter => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.currently_editing = Some(CurrentlyEditing::Value);
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.save_key_value();
+                                    app.current_screen = CurrentScreen::Main;
+                                }
+                            }
+                        }
+                    }
+                    KeyCode::Backspace => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.key_input.pop();
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.value_input.pop();
+                                }
+
+                                
+                            }
+                        }
+                    }
+                },
+            }
+        }
+    }
 }
